fedb8233eed5567d33e4464e981d609b1ab24b17
Joshua Chin <joshuarchin@gmail.com>
2014-06-17 20:08:47 -0400

    factored out drawing

--- graphics.js
+++ graphics.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var gl, shaderProgram, buffer, vertexPositionAttribute;
-var cameraAttr, modelAttr;
+var gl, shaderProgram, buffer;
+var vPosAttr, cameraAttr, modelAttr;
 
 function run() {
 	var canvas = document.getElementById("glcanvas");
@@ -14,23 +14,24 @@ function run() {
 
 	cameraAttr = gl.getUniformLocation(shaderProgram, "u_cameraMatrix");
 	modelAttr = gl.getUniformLocation(shaderProgram, "u_modelMatrix");
-	
+
 	buffer = initBuffers();
 	setInterval(render, 15);
 }
 
 function render() {
-	var pos = vertexPositionAttribute;
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-	gl.vertexAttribPointer(pos, 3, gl.FLOAT, 3, 0, 0);
-
 	setCamera(gl, cameraAttr, Math.PI / 4, 640.0 / 480.0, 0.1, 100);
+	draw(buffer, matrix.translate(0, 0, -6));
 
-	gl.uniformMatrix4fv(modelAttr, false, new Float32Array(matrix.translate(0.0, 0.0, -6.0).flatten()));
+}
+
+function draw(vbo, modelMatrix) {
+	gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+	gl.vertexAttribPointer(vPosAttr, 3, gl.FLOAT, 3, 0, 0);
+	gl.uniformMatrix4fv(modelAttr, false, new Float32Array(modelMatrix.flatten()));
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-
 }
 
 function initBuffers() {
