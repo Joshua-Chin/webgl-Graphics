9b22cd7c95781a832b620f1d186ade92509cc4c3
Joshua Chin <joshuarchin@gmail.com>
2014-06-17 20:29:15 -0400

    added path rendering

--- graphics.js
+++ graphics.js
@@ -15,27 +15,55 @@ function run() {
 	cameraAttr = gl.getUniformLocation(shaderProgram, "u_cameraMatrix");
 	modelAttr = gl.getUniformLocation(shaderProgram, "u_modelMatrix");
 
-	squareBuffer = genBuffer([1.0, 1.0, 0.0, -1.0, 1.0, 0.0, 1.0, -1.0, 0.0, -1.0, -1.0, 0.0]);
+	squareBuffer = genObjectBuffer(
+		[1.0, 1.0, 0.0,
+		-1.0, 1.0, 0.0,
+		1.0, -1.0, 0.0,
+		-1.0, 1.0, 0.0,
+		-1.0, -1.0, 0.0,
+		1.0, -1.0, 0.0,]);
 	setInterval(render, 15);
 }
 
 function render() {
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	setCamera(gl, cameraAttr, Math.PI / 4, 640.0 / 480.0, 0.1, 100);
-	draw(squareBuffer, matrix.translate(0, 0, -6));
-
+	drawObject(squareBuffer, matrix.translate(0, 0, -6), 6);
 }
 
-function draw(vbo, modelMatrix) {
+function drawObject(obj, modelMatrix) {
+	var vbo = obj.buffer;
+	var length = obj.length;
 	gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
 	gl.vertexAttribPointer(vPosAttr, 3, gl.FLOAT, 3, 0, 0);
 	gl.uniformMatrix4fv(modelAttr, false, new Float32Array(modelMatrix.flatten()));
-	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+	gl.drawArrays(gl.TRIANGLES, 0, length);
 }
 
-function genBuffer(vertices) {
+function genObjectBuffer(triangles) {
 	var buffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-	return buffer;
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangles), gl.STATIC_DRAW);
+	return {buffer:buffer, length:triangles.length/3};
+}
+
+function drawPath(path, modelMatrix) {
+	var vbo = path.buffer;
+	var length = path.length;
+	gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+	gl.vertexAttribPointer(vPosAttr, 3, gl.FLOAT, 3, 0, 0);
+	gl.uniformMatrix4fv(modelAttr, false, new Float32Array(modelMatrix.flatten()));
+	gl.drawArrays(gl.LINE_STRIP, 0, length);
+}
+
+function genPathBuffer(func, granularity) {
+	points = [];
+	for(var i=0; i<granularity; i++){
+		points[i] = func(i/(granularity-1));
+	}
+	
+	var buffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points), gl.STATIC_DRAW);
+	return {buffer:buffer, length:granularity};
 }
